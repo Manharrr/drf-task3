7f8946e1770e580691d44307eac2055b9627b7a6
ManharCK <manhargurukkal2@gmail.com>
2026-04-26 22:34:37 +0530

    updated

--- students/urls.py
+++ students/urls.py
@@ -1,7 +1,6 @@
 # students/urls.py
 from django.urls import path
 from .views import StudentListCreateView, StudentDetailView
-
 urlpatterns = [
     path('students/', StudentListCreateView.as_view()),
     path('students/<int:pk>/', StudentDetailView.as_view()),
